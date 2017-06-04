0ff5990014e96bcd437c35aaaa1ae6799c4fb832
uhilari <uhilari@gmail.com>
2017-06-04 10:29:45 -0500

    reorder mapper

---
+++ fuente/HS.Comun.Dominio/Entidades/Datos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HS.Comun.Dominio.Entidades
+{
+  public class Datos: EntityBase
+  {
+    public virtual string Codigo { get; set; }
+    public virtual string Descripcion { get; set; }
+  }
+}
